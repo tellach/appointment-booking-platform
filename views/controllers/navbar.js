e724295b18c02f5931711cd7784f48aef09a4812
adelnamani <ga_namani@esi.dz>
2020-04-20 05:03:08 +0100

    updates

--- views/controllers/navbar.js
+++ views/controllers/navbar.js
@@ -1,14 +1,12 @@
 
 function updateNavBar (){
     let data = require('../config.json')
-    console.log(data.doctor_name)
     document.getElementById('doctor-name').innerHTML = data.doctor_name
 }
 
 document.addEventListener("DOMContentLoaded", function(){
     updateNavBar()
     ipc.on('updatedConfigs',()=>{
-        console.log('udated confid rec !')
         updateNavBar()
     })
 })
